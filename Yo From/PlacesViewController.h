edc26d8fd1a5aaba1aaeef8f95aaecb4eb34103d
Spencer Yen <spencersyen@gmail.com>
2014-07-16 00:17:25 -0700

    Added new files

---
+++ Yo From/PlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlacesViewController.h
+//  Yo From
+//
+//  Created by Spencer Yen on 7/16/14.
+//  Copyright (c) 2014 Aakash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesViewController : UIViewController
+
+@end
---
+++ Yo From/UsersViewController.h
@@ -0,0 +1,13 @@
+//
+//  UsersViewController.h
+//  Yo From
+//
+//  Created by Spencer Yen on 7/16/14.
+//  Copyright (c) 2014 Aakash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UsersViewController : UIViewController
+
+@end
